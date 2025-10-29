fbd383636856911a638c895a606c9821e9884c0e
Reece Naicker <genesisprepaidsolutions@gmail.com>
2025-10-29 14:01:45 +0200

    Update app.py

--- smart-meter-stock-management-manager/app.py
+++ smart-meter-stock-management-manager/app.py
@@ -23,7 +23,7 @@ GREY = "#F5F7FA"
 # ====================================================
 st.set_page_config(
     page_title="Acucomm Stock Management",
-    page_icon="favivon.jpg",   
+    page_icon="favicon.jpg",   
     layout="centered"
 )
 
